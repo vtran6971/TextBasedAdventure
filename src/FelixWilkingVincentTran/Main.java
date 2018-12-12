5481729f52faaec83963fe8c21cf5a585f5bbbd0
BT_1N3_16 <fwilking4736@bths.edu>
2018-12-12 08:41:26 -0500

    ghf

--- src/FelixWilkingVincentTran/Main.java
+++ src/FelixWilkingVincentTran/Main.java
@@ -5,10 +5,15 @@ public class Main {
     public static void main(String[] args) {
         int boardlength = 20;
         int difficulty = 50;
+
+        int x = 0;
+        int y = 0;
+
         Boardobject[][] boardarray = new Boardobject[boardlength][boardlength];
         for(int i = 0; i < difficulty; i++) {
             boardarray[(int) (Math.random() * boardlength)][(int) (Math.random() * boardlength)] = new planet(3);
         }
+        boardarray[x][y] = new playerclass("Hello");
         Boardclass board = new Boardclass(boardarray);
         board.printboard();
     }
